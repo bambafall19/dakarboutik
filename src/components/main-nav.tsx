dbcbf4c9e075ab53316fb74b29d7f49a89492a42
dakarboutik <mmddakarboutik@gmail.com>
2026-01-12 16:05:17 +0000

    Try fixing this error: `Console Error: In HTML, <a> cannot be a descenda

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -14,7 +14,6 @@ import {
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu';
-import { CategoryIcons, Icons } from './icons';
 
 interface MainNavProps {
   items: Category[];
@@ -64,26 +63,29 @@ export function MainNav({ items }: MainNavProps) {
 }
 
 const ListItem = React.forwardRef<
-  React.ElementRef<'a'>,
-  React.ComponentPropsWithoutRef<'a'>
->(({ className, title, children, ...props }, ref) => {
+  HTMLLIElement,
+  {
+    className?: string;
+    title: string;
+    href: string;
+    children?: React.ReactNode;
+  }
+>(({ className, title, children, href, ...props }, ref) => {
   return (
-    <li>
+    <li
+      ref={ref}
+      className={cn(
+        'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+        className
+      )}
+      {...props}
+    >
       <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
-            className
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          {children && <div className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </div>}
-        </a>
+        <Link href={href} className="font-medium leading-none">{title}</Link>
       </NavigationMenuLink>
+      {children && <div className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+        {children}
+      </div>}
     </li>
   );
 });

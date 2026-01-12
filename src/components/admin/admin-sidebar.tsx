4209e05cf7ab0eb38907c4087992ddfe02fc896e
dakarboutik <mmddakarboutik@gmail.com>
2026-01-12 13:06:15 +0000

    Try fixing this error: `Console Error: In HTML, <a> cannot be a descenda

--- src/components/admin/admin-sidebar.tsx
+++ src/components/admin/admin-sidebar.tsx
@@ -55,10 +55,10 @@ export function AdminSidebar() {
     <div className="hidden border-r bg-card lg:block">
       <div className="flex h-full max-h-screen flex-col gap-2">
         <div className="flex h-14 items-center border-b px-6">
-          <Link href="/admin" className="flex items-center gap-2 font-semibold">
+          <div className="flex items-center gap-2 font-semibold">
             <Logo />
             <span className="">Admin</span>
-          </Link>
+          </div>
         </div>
         <div className="flex-1 overflow-y-auto">
           <nav className="grid items-start px-4 text-sm font-medium">

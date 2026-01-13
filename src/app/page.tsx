35cc58807e3e84a16c913f53e7e79f0922ca1b1a
dakarboutik <mmddakarboutik@gmail.com>
2026-01-13 14:32:46 +0000

    et ici vu que il occupe bcp espace sur la version mobile (_for element <

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,11 +26,11 @@ export default function HomePage() {
       <HeroSection />
       
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <aside className="lg:col-span-3">
+        <aside className="hidden lg:block lg:col-span-3">
           <HomeSidebar />
         </aside>
         
-        <main className="lg:col-span-9 space-y-12 md:space-y-16">
+        <main className="col-span-12 lg:col-span-9 space-y-12 md:space-y-16">
             {loading ? (
               <>
                 <div className="grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-6">

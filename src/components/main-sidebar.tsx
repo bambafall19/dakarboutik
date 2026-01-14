8e5fe0ff5102dca4469ba95f727d1358e05f866c
dakarboutik <mmddakarboutik@gmail.com>
2026-01-14 09:44:05 +0000

    Try fixing this error: `Runtime ReferenceError: useSearchParams is not d

--- src/components/main-sidebar.tsx
+++ src/components/main-sidebar.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { usePathname, useSearchParams } from 'next/navigation';
 import { Icons, CategoryIcons } from './icons';
 import { Button } from './ui/button';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './ui/tooltip';

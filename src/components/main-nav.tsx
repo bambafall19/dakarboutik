e51c51d2f6c5a566b78af34f7b4c46751be4e31f
dakarboutik <mmddakarboutik@gmail.com>
2026-01-15 13:33:19 +0000

    Try fixing this error: `Console Error: `legacyBehavior` is deprecated an

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -24,7 +24,7 @@ export function MainNav({ items }: MainNavProps) {
     <NavigationMenu>
       <NavigationMenuList>
         <NavigationMenuItem>
-          <Link href="/products" legacyBehavior passHref>
+          <Link href="/products" legacyBehavior={false} passHref={false} asChild>
             <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "bg-transparent")}>
               Shop
             </NavigationMenuLink>
@@ -47,14 +47,14 @@ export function MainNav({ items }: MainNavProps) {
           </NavigationMenuContent>
         </NavigationMenuItem>
         <NavigationMenuItem>
-           <Link href="/sav" legacyBehavior passHref>
+           <Link href="/sav" legacyBehavior={false} passHref={false} asChild>
             <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "bg-transparent")}>
               Garantie & SAV
             </NavigationMenuLink>
           </Link>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <Link href="/admin" legacyBehavior passHref>
+          <Link href="/admin" legacyBehavior={false} passHref={false} asChild>
             <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "bg-transparent")}>
               Admin
             </NavigationMenuLink>
@@ -90,4 +90,3 @@ const ListItem = React.forwardRef<
   );
 });
 ListItem.displayName = 'ListItem';
-

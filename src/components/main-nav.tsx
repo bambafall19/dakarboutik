ecb717e67c5fe7c21467f8ce7bf09768a3a21b5e
dakarboutik <mmddakarboutik@gmail.com>
2026-01-15 16:23:42 +0000

    ## Error Type Console Error
    ## Error Message
    `legacyBehavior` is deprec

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -26,7 +26,7 @@ export function MainNav({ items }: MainNavProps) {
     <NavigationMenu>
       <NavigationMenuList>
         <NavigationMenuItem>
-          <Link href="/products" legacyBehavior passHref>
+           <Link href="/products" legacyBehavior passHref>
             <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "bg-transparent text-nav-foreground hover:bg-white/10 focus:bg-white/10 text-xs")}>
                 BOUTIQUE
             </NavigationMenuLink>

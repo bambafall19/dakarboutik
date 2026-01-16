ddbdbb25a265caabc596eae7a6d26d41d55a6ea9
dakarboutik <mmddakarboutik@gmail.com>
2026-01-16 12:46:54 +0000

    Installer Google Tag Manager Copiez le code ci-dessous et collez-le sur

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,17 @@ export default function RootLayout({
   return (
     <html lang="fr-SN" className="h-full" suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NGD258WC');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <title>DakarBoutik</title>
         <meta
           name="description"
@@ -42,6 +53,14 @@ export default function RootLayout({
           ubuntu.variable
         )}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NGD258WC"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+        {/* End Google Tag Manager (noscript) */}
         <ThemeProvider
           attribute="class"
           defaultTheme="system"

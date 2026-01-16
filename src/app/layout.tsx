d4c5ad2bf4631e82f9d01985b16225931c192aca
dakarboutik <mmddakarboutik@gmail.com>
2026-01-16 12:36:15 +0000

    voici

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,15 @@ export default function RootLayout({
   return (
     <html lang="fr-SN" className="h-full" suppressHydrationWarning>
       <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer', '${GTM_ID}');
+          `}
+        </Script>
         <title>DakarBoutik</title>
         <meta
           name="description"
@@ -61,16 +70,6 @@ export default function RootLayout({
             </CartProvider>
           </FirebaseClientProvider>
         </ThemeProvider>
-
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer', '${GTM_ID}');
-          `}
-        </Script>
       </body>
     </html>
   );

af09e431bc28006e4fab3df291189a07eefb3354
dakarboutik <mmddakarboutik@gmail.com>
2026-01-23 12:59:59 +0000

    des manquement sur la pwa ??

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         />
         <meta name="google-site-verification" content="KFsMq_Wa2CFdELHtlc2fH1m9MiyOpvThvdC1kc9BqTk" />
         
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="https://storage.googleapis.com/studioprod-52595.appspot.com/dakarboutik/logo.png?v=2" sizes="any" type="image/png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#112D55" />

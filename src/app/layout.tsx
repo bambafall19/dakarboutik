6226bf72ca222ba730ee4dc39739989358ce48c1
dakarboutik <mmddakarboutik@gmail.com>
2026-01-26 16:21:52 +0000

    Installer Google Tag Manager Copiez le code ci-dessous et collez-le sur

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NGD258WC');
+})(window,document,'script','dataLayer','GTM-N2FPSJ7P');
           `}
         </Script>
         <title>DakarBoutik</title>
@@ -83,7 +83,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       >
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NGD258WC"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N2FPSJ7P"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}

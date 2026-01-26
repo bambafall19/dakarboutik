c72dbf1dc90291b753f5e775360069092f2a06ab
dakarboutik <mmddakarboutik@gmail.com>
2026-01-26 16:24:01 +0000

    Votre balise Google n'a pas été détectée sur votre site Web.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { Ubuntu, DynaPuff } from 'next/font/google';
-import Script from 'next/script';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import { Toaster } from '@/components/ui/toaster';
@@ -50,15 +49,15 @@ export default function RootLayout({
   return (
     <html lang="fr-SN" className="h-full" suppressHydrationWarning>
       <head>
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N2FPSJ7P');
-          `}
-        </Script>
+})(window,document,'script','dataLayer','GTM-N2FPSJ7P');`,
+          }}
+        />
         <title>DakarBoutik</title>
         <meta
           name="description"
